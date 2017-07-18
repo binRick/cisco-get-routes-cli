26e1b95e80b33c95bde477f4dd907e4c968e5b96
binRick <rapidvps@gmail.com>
2017-07-18 20:17:34 +0000

    security optimization

--- index.js
+++ index.js
@@ -17,22 +17,22 @@ var Client = require('ssh2').Client,
 
 config.secret = process.env['PASS_SECRET'];
 
-if (!config.secret || config.secret.length < 1) {
-    config.secret = prompt("Secret: ", {
-        secure: true
-    });
-	console.log(c.green('Run this command and rerun the getRoute command...'));
-	console.log('export PASS_SECRET="'+config.secret+'"');
-	process.exit();
-    process.env['PASS_SECRET'] = config.secret;
-}
-cryptr = new Cryptr(config.secret);
-
 var ip = process.argv[2];
 if (!ip || !validateip(ip)) {
     console.log(c.red('First argumennt needs to be an IP address'));
     process.exit(-1);
 }
+if (!config.secret || config.secret.length < 1) {
+    config.secret = prompt("Secret: ", {
+        secure: true
+    });
+	console.log(c.green('Run this command and rerun the getRoute command...'));
+	console.log('export PASS_SECRET="'+config.secret+'" && getRoutes '+ip);
+	process.exit();
+    process.env['PASS_SECRET'] = config.secret;
+}
+cryptr = new Cryptr(config.secret);
+
 
 var rcmd = 'sh ip route ' + ip;
 var getOutput = function(host, cmd, _cb) {
@@ -45,7 +45,7 @@ var getOutput = function(host, cmd, _cb) {
                 setTimeout(function() {
                     conn.end();
                     _cb(err, dat);
-                }, 10);
+                }, 30);
             }).on('data', function(data) {
                 dat += data.toString();
             }).stderr.on('data', function(data) {}).on('error', function(error) {});
@@ -53,8 +53,8 @@ var getOutput = function(host, cmd, _cb) {
                 stream.write(cmd + '\n');
                 setTimeout(function() {
                     stream.end('exit\n');
-                }, 10);
-            }, 10);
+                }, 30);
+            }, 30);
         });
     }).connect({
         host: host,
